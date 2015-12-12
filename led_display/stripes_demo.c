449ffec45f0d1d7cb479ac70e7393c404aca8077
Vince Weaver <vincent.weaver@maine.edu>
2015-12-12 15:54:36 -0500

    display: make stripes demo mildly more interesting

--- led_display/stripes_demo.c
+++ led_display/stripes_demo.c
@@ -7,15 +7,18 @@
 
 int stripes_demo(int offset) {
 
-	int i;
-
-	for(i=0;i<8;i++) {
-		hline(0,7,i,i&3);
-	}
+	int i,off=offset;
 
 	while(1) {
+		for(i=0;i<8;i++) {
+			hline(0,7,i,(i+off)&3);
+		}
+
 		sync_display();
 		sleep(1);
+
+		off++;
+		if (off>7) off=0;
 	}
 
 	return 0;
