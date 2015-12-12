a4dd7b4cd4b4e30b67a991571f1a6d5bd00b897f
Vince Weaver <vincent.weaver@maine.edu>
2015-12-12 16:34:36 -0500

    display: initial command sending tool

---
+++ led_display/send_command.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+/*
+	Commands:
+		Lines
+		Sine
+		CPU Load
+		Memory Load
+		Scroll Text
+		Temperature
+		Bouncing Ball
+		Blinkenlights
+		Node ID
+		Shutdown
+
+
+
+*/
+
+	return 0;
+}
